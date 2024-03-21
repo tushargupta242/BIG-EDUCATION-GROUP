fa0af47c27e3c10f3540b876a1a3a99cd25a6478
TUSHAR GUPTA <guptatushar529@gmail.com>
2024-03-21 14:17:22 +0530

    ABOUT PAGE

--- script.js
+++ script.js
@@ -24,5 +24,23 @@ else
     })
 })
 
+let valueDispalys = document.querySelectorAll(".num");
+let interval = 2000;
+valueDispalys.forEach((valueDispaly) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDispaly.getAttribute("data-val"));
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(function()
+    {
+        startValue += 1;
+        valueDispaly.textContent = startValue;
+        if(startValue == endValue)
+        {
+            clearInterval(counter);
+        }
+    },duration);    
+    });
+
+ 
 
 
